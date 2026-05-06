386e9c8aef5f497b43142f6da49f7b61a052835e
Igor Franca <igrfranca@gmail.com>
2026-05-06 14:16:21 -0300

    Interfaces

---
+++ src/dio/java/interfaces/MusicPlayer.java
@@ -0,0 +1,16 @@
+package dio.java.interfaces;
+
+public interface MusicPlayer {
+
+  //Public Static Void
+  String music = "Parabéns para você";
+
+  void playMusic();
+
+  void pauseMusic();
+
+  void stopMusic();
+
+
+
+}
---
+++ src/dio/java/interfaces/VideoPlayer.java
@@ -0,0 +1,12 @@
+package dio.java.interfaces;
+
+public interface VideoPlayer {
+
+  void playVideo();
+
+  void pauseVideo();
+
+  void stopVideo();
+
+
+}
